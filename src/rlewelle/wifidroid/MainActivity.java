276f2f293f55879fce0f81a095b4aa9f9a3d03d4
Ryan Lewellen <rlewelle@mines.edu>
2012-12-03 21:33:39 -0700

    Custom row views for ScanResult items
    - Still need to convert frequency to specific channel, strength to some
    sort of meter

--- src/rlewelle/wifidroid/MainActivity.java
+++ src/rlewelle/wifidroid/MainActivity.java
@@ -8,10 +8,13 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -24,29 +27,59 @@ public class MainActivity extends ListActivity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.main);
 
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        Log.d("MainActivity.onCreate", wifi.toString());
 
         List<ScanResult> scanResults = wifi.getScanResults();
-
         if (scanResults == null) {
             Log.d("MainActivity.onCreate", "scanResults is NULL");
             return;
         }
 
-        Log.d("MainActivity.onCreate", scanResults.toString());
-        for (ScanResult result : scanResults) {
-            Log.d("MainActivity.onCreate", result.toString());
+        NetworkListAdapter scanAdapter = new NetworkListAdapter(scanResults);
+        setListAdapter(scanAdapter);
+    }
+
+    public class NetworkListAdapter extends ArrayAdapter<ScanResult> {
+        public class NetworkListRowHolder {
+            TextView ssid;
+            TextView seen;
+            TextView strength;
+            TextView channel;
+
+            public NetworkListRowHolder(View view) {
+                ssid = (TextView)view.findViewById(R.id.network_ssid);
+                seen = (TextView)view.findViewById(R.id.network_lastseen);
+                strength = (TextView)view.findViewById(R.id.network_strength);
+                channel = (TextView)view.findViewById(R.id.network_channel);
+            }
+
+            public void hydrate(ScanResult scan) {
+                ssid.setText(scan.SSID);
+                seen.setText(Long.toString(scan.timestamp));
+                strength.setText(Integer.toString(scan.level));
+                channel.setText(Integer.toString(scan.frequency));
+            }
         }
 
-        ArrayAdapter<ScanResult> scanAdapter = new ArrayAdapter<ScanResult>(
-                this,
-                android.R.layout.simple_list_item_1,
-                scanResults
-        );
+        public NetworkListAdapter(List<ScanResult> scanResults) {
+            super(MainActivity.this, R.layout.wifi_list_row, R.id.network_ssid, scanResults);
+        }
 
-        setListAdapter(scanAdapter);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            NetworkListRowHolder holder;
+
+            if (convertView == null || convertView.getTag() == null) {
+                convertView = getLayoutInflater().inflate(R.layout.wifi_list_row, null);
+                holder = new NetworkListRowHolder(convertView);
+                convertView.setTag(holder);
+            } else {
+                holder = (NetworkListRowHolder)convertView.getTag();
+            }
+
+            holder.hydrate(getItem(position));
+            return convertView;
+        }
     }
 }

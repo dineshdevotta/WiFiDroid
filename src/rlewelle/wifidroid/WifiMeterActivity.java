808335385f3bf3ecd6ad047b36cfc581aec05704
Ryan Lewellen <rlewelle@mines.edu>
2012-12-10 20:37:49 -0700

    Exploration into AChartEngine

--- src/rlewelle/wifidroid/WifiMeterActivity.java
+++ src/rlewelle/wifidroid/WifiMeterActivity.java
@@ -4,10 +4,20 @@ import android.app.Activity;
 import android.content.*;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Pair;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.BarChart;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import rlewelle.wifidroid.data.AccessPoint;
+import rlewelle.wifidroid.data.AccessPointDataPoint;
 
 public class WifiMeterActivity extends Activity implements DataService.IDataServicable{
     public static final String EXTRA_AP = "rlewelle.wifidroid.wifimeteractivity.EXTRA_AP";
@@ -23,6 +33,28 @@ public class WifiMeterActivity extends Activity implements DataService.IDataServ
         setContentView(R.layout.wifi_meter);
         holder = new WifiMeterHolder();
         serviceLink.onCreate();
+
+        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+
+        XYSeries a = new XYSeries("A");
+        a.add(1.0, 10.0);
+        a.add(2.0, 20.0);
+        a.add(3.0, 5.0);
+
+        dataset.addSeries(a);
+
+        XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+        renderer.addSeriesRenderer(new SimpleSeriesRenderer());
+
+        GraphicalView graph = ChartFactory.getBarChartView(
+            this,
+            dataset,
+            renderer,
+            BarChart.Type.DEFAULT
+        );
+
+        FrameLayout graphHost = (FrameLayout) findViewById(R.id.network_signal_graph_frame);
+        graphHost.addView(graph);
 
         ap = getIntent().getParcelableExtra(EXTRA_AP);
     }
@@ -67,7 +99,8 @@ public class WifiMeterActivity extends Activity implements DataService.IDataServ
         if (ap == null)
             return;
 
-        holder.hydrate(ap);
+        Pair<Long, AccessPointDataPoint> dp = serviceLink.getService().getLatestResult(ap);
+        holder.hydrate(ap, dp.first, dp.second);
     }
 
     private class WifiMeterHolder {
@@ -79,7 +112,7 @@ public class WifiMeterActivity extends Activity implements DataService.IDataServ
             SSID  = (TextView) findViewById(R.id.network_ssid);
         }
 
-        public void hydrate(AccessPoint ap) {
+        public void hydrate(AccessPoint ap, Long lastSeen, AccessPointDataPoint dp) {
             BSSID.setText(ap.getBSSID());
             SSID.setText(ap.getSSID());
         }
